9494e4a622ab0392ce9124a55658e5cfc4c67dea
arvind chaudhary <chaudhary003@gmail.com>
2020-02-10 18:17:41 +0530

    admin and registeration completed

--- wordcounter/settings.py
+++ wordcounter/settings.py
@@ -1,5 +1,5 @@
 import os
-
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +119,8 @@ STATICFILES_DIRS=[
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
